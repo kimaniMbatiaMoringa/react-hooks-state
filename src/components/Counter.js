d0c6a545105dc18f28a8ce4530f4426fea303b90
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-07-22 17:44:43 +0300

    Completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,40 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("red")
 
   function increment() {
     setCount(count + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  function changeColorToBlue(){
+    setColor("Blue")
+  }
+
+  function Counter() {
+    const [count, setCount] = useState(0);
+  
+    function increment() {
+      console.log(`before setState: ${count}`);
+  
+      setCount(count + 1);
+  
+      console.log(`after setState: ${count}`);
+    }
+  
+    return <div onClick={increment}>{count}</div>;
+  }
+
+  return( 
+  <>
+      <button onClick={increment}>I have been clicked {count} times</button>
+      <h2>My favorite color is {color}</h2>
+      <button onClick={changeColorToBlue}>Change to Blue</button>
+      
+      <Counter />
+  </>
+
+  )
 }
 
 export default Counter;
